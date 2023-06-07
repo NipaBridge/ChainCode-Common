1e07b922853cb7142619ec4ede6ea3444ba37088
jinsan74 <jinsan@sejongtelecom.net>
2023-06-07 16:16:45 +0900

    DoTransfer 함수 추가

--- utils/comFunction.go
+++ utils/comFunction.go
@@ -17,6 +17,24 @@ import (
 )
 
 // DoTransfer is 토큰 Transfer
+func DoTransfer(stub shim.ChaincodeStubInterface, transParam string, tokenName string) sc.Response {
+
+	_, orgParam := stub.GetFunctionAndParameters()
+
+	walletMeta := wallet.WalletMeta{}
+	json.Unmarshal([]byte(orgParam[0]), &walletMeta)
+	walletMeta.Transdata = transParam
+
+	realTrans, _ := json.Marshal(walletMeta)
+
+	chainCodeFunc := "transfer"
+	invokeArgs := ToChaincodeArgs(chainCodeFunc, string(realTrans))
+	channel := stub.GetChannelID()
+	response := stub.InvokeChaincode(tokenName, invokeArgs, channel)
+
+	return response
+}
+
 // DoBalanceOf is 토큰 balanceOf
 func DoBalanceOf(stub shim.ChaincodeStubInterface, toaddress string, tokenName string) sc.Response {
 
